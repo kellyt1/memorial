b5cfe742b43730a09171b75c8583cbc49156a9bf
kellyt1 <tossy.kelly@gmail.com>
2016-02-09 18:28:00 -0600

    app stop working after creating controllers

--- grails-app/controllers/com/memorial/MemorialController.groovy
+++ grails-app/controllers/com/memorial/MemorialController.groovy
@@ -40,7 +40,7 @@ class MemorialController {
         request.withFormat {
             form multipartForm {
                 flash.message = message(code: 'default.created.message', args: [message(code: 'memorial.label', default: 'Memorial'), memorial.id])
-                redirect memorial
+                 redirect memorial
             }
             '*' { respond memorial, [status: CREATED] }
         }
